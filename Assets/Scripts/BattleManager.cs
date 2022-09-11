fb518f5011d4d704f31d35d605f9e7cf1eaff3c9
JonathanAllenRay <jonathanallenray1@gmail.com>
2022-09-10 21:03:33 -0500

    Added comments to several classes

--- Assets/Scripts/BattleManager.cs
+++ Assets/Scripts/BattleManager.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-
+//test
 public class BattleManager : MonoBehaviour
 {
     // Start is called before the first frame update
